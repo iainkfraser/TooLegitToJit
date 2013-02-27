05d8d44e68e3bc17a4cdb27861eef8e85b5e7546
iain <iain@mipsvm.(none)>
2013-02-27 22:24:16 +0000

    Did large chunk of generic xlogue code. Untested

--- xlogue.c
+++ xlogue.c
@@ -14,112 +14,139 @@
 #include "machine.h"
 #include "lopcodes.h"
 #include "synthetic.h"
-
-static void emit_load_params( struct machine_ops* mop, struct emitter* me, struct frame* f, const int reg_value, const int reg_count );
+#include "lua.h"
+#include "stack.h"
 
 static int stack_frame_size( int nr_locals ){
 	int k = 4 * 4;	// ra, closure, return position, nr results 
 	return nr_locals * 8 + k;
 }
 
-void epilogue( struct machine_ops* mop, struct emitter* e, struct frame* f ){
-#if 0
-	const int rsp = f->m->sp;
-	operand osp = OP_TARGETREG( rsp );
+/* Grab non temps first then temps */
+static void prefer_nontemp_acquire_reg( struct machine_ops* mop, struct emitter* e, struct machine* m, 
+					int n, operand reg[n] ){
+	const int nr_temps = m->nr_temp_regs;
+	const int nr_nontemps = m->nr_reg - nr_temps;
 
-	int temp1 = acquire_specfic_temp( f->m, 0 );
-	int temp2 = acquire_specfic_temp( f->m, 1 );
-
-	mop->add( e, f->m, osp, osp, OP_TARGETIMMED( stack_frame_size( f->nr_locals ) ) ); 
-	mop->move( e, f->m, OP_TARGETREG( temp1 ), OP_TARGETDADDR( rsp, -4 ) ); 
-	mop->move( e, f->m, OP_TARGETREG( temp2 ), OP_TARGETDADDR( rsp, -8 ) ); 
-
-	release_specfic_temp( f->m, 0 );
-	release_specfic_temp( f->m, 1 );
-
-	mop->ret( e, f->m );
-#endif
+	for( int i = 0; i < n; i++ ){
+		if( i < nr_nontemps )
+			reg[i] = OP_TARGETREG( nr_temps + i );
+		else
+			reg[i] = OP_TARGETREG( acquire_temp( mop, e, m ) );	// ( i - nr_nontemps );	
+	}
 }
 
+static void prefer_nontemp_release_reg( struct machine_ops* mop, struct emitter* e, struct machine* m, int n ){
+	const int nr_temps = m->nr_temp_regs;
+	const int nr_nontemps = m->nr_reg - nr_temps;
+
+	for( int i = nr_nontemps; i < n; i++ ){
+		release_temp( mop, e, m );
+	}
+}
+
+
+// in order of nontemporary register assignment priority 
+enum REGARGS { RA_NR_ARGS, RA_BASE, RA_NR_RESULTS, RA_CLOSURE, RA_COUNT };
+
+static void load_args( struct machine_ops* mop, struct emitter* e, struct frame* f, operand rargs[ RA_COUNT ] ){
+	operand sp = OP_TARGETREG( f->m->sp );
+	operand t0 = OP_TARGETREG( acquire_temp( mop, e, f->m ) );
+
+	/*
+	* n = nr_locals 
+	* f(i) = sp + 8( n - ( i + 1 ) )		// argument addr in new frame 
+	* g(i) = base - 8i 				// argument addr in old frame
+	* f(i) - g(i) = delta = sp - base + 8( n - 1 )	
+	*/
+
+	// calculate f( NR_PARAMS - 1 )
+	mop->add( e, f->m, t0, sp, OP_TARGETIMMED( 8 * ( f->nr_locals - f->nr_params ) ) );
+
+	// base = g( NR_ARGS - 1 );  NR_ARGS = f( NR_ARGS - 1 );
+	mop->mul( e, f->m, rargs[ RA_NR_ARGS ], rargs[ RA_NR_ARGS ], OP_TARGETIMMED( 8 ) );
+	mop->add( e, f->m, rargs[ RA_BASE ], rargs[ RA_BASE ], rargs[ RA_NR_ARGS ] );
+	mop->add( e, f->m, rargs[ RA_BASE ], rargs[ RA_BASE ], OP_TARGETIMMED( -8 ) );
+	mop->add( e, f->m, rargs[ RA_NR_ARGS ], rargs[ RA_NR_ARGS ], sp );
+	mop->add( e, f->m, rargs[ RA_NR_ARGS ], rargs[ RA_NR_ARGS ], OP_TARGETIMMED( 8 * f->nr_locals ) );
+
+
+	// loop storing nil in type 
+	e->ops->label_local( e, 0 );
+	mop->bge( e, f->m, t0, rargs[ RA_NR_ARGS ], LBL_NEXT( 0 )  );
+	mop->move( e, f->m, OP_TARGETDADDR( t0.reg, 4 ), OP_TARGETIMMED( LUA_TNIL ) ); 
+	mop->add( e, f->m, t0, t0, OP_TARGETIMMED( 8 ) ); 
+	mop->b( e, f->m, LBL_PREV( 0 ) );
+	e->ops->label_local( e, 0 );
+
+	// NR_ARGS = f( 0 ), not needed anymore
+	mop->add( e, f->m, rargs[ RA_NR_ARGS ], sp, OP_TARGETIMMED( 8 * ( f->nr_params - 1 ) ) ); 
+
+	
+	// copy args in reverse order ( avoid overwrite )  
+	e->ops->label_local( e, 0 );
+	mop->bge( e, f->m, t0, rargs[ RA_NR_ARGS ], LBL_NEXT( 0 ) );
+
+	mop->move( e, f->m, OP_TARGETDADDR( t0.reg, 0 ), OP_TARGETDADDR( rargs[ RA_BASE ].reg, 0 ) );
+	mop->move( e, f->m, OP_TARGETDADDR( t0.reg, 4 ), OP_TARGETDADDR( rargs[ RA_BASE ].reg, 4 ) );
+
+	mop->add( e, f->m, rargs[ RA_BASE ], rargs[ RA_BASE ], OP_TARGETIMMED( 8 ) );	
+	mop->add( e, f->m, t0, t0, OP_TARGETIMMED( 8 ) ); 
+	mop->b( e, f->m, LBL_PREV( 0 ) );
+	e->ops->label_local( e, 0 );
+}
 
 void prologue( struct machine_ops* mop, struct emitter* e, struct frame* f ){
-#if 0
-	const int rsp = f->m->sp;
-	operand osp = OP_TARGETREG( rsp );
+	const int meta_off = f->nr_locals * 8;
 
-	// save closure, result address and expected number of results 
-	int temp1 = acquire_specfic_temp( f->m, 0 ); 
-	int temp2 = acquire_specfic_temp( f->m, 1 ); 
-	int temp3 = acquire_specfic_temp( f->m, 2 ); 
+	// new frame assumes no temporaries have been used yet 
+	assert( temps_accessed( f->m ) == 0 );
+	assert( RA_COUNT <= f->m->nr_reg );		// regargs are passed by register NOT stack
+
+	// first things first sub stack
+	operand sp = OP_TARGETREG( f->m->sp );
+	mop->add( e, f->m, sp, sp, OP_TARGETIMMED( -stack_frame_size( f->nr_locals ) ) ); 
+
+	// get arg passing registers
+	operand rargs[ RA_COUNT ];
+	prefer_nontemp_acquire_reg( mop, e, f->m, RA_COUNT, rargs );
+
+	// do this first its very delicate 
+	load_args( mop, e, f, rargs );
+
+	// store metaframe section
+	mop->move( e, f->m, OP_TARGETDADDR( f->m->sp, meta_off ), rargs[ RA_NR_RESULTS ] );
+	mop->move( e, f->m, OP_TARGETDADDR( f->m->sp, meta_off + 4 ), rargs[ RA_BASE ] );
+	mop->move( e, f->m, OP_TARGETDADDR( f->m->sp, meta_off + 8 ), rargs[ RA_CLOSURE ] );
+
+	release_temp( mop, e, f->m );
+
+	// load live registers  
+	load_frame( mop, e, f );
+}
+
+void epilogue( struct machine_ops* mop, struct emitter* e, struct frame* f ){
+	const int meta_off = f->nr_locals * 8;
 	
-	// load the paramaters, while still in old stack frame  
-	emit_load_params( mop, e, f, temp2, temp3 );
+	// new frame assumes no temporaries have been used yet 
+	assert( temps_accessed( f->m ) == 0 );
+	assert( RA_COUNT <= f->m->nr_reg );		// regargs are passed by register NOT stack
 
-	mop->move( e, f->m, OP_TARGETDADDR( rsp, -4 ), OP_TARGETREG( temp1 ) ); 
-	mop->move( e, f->m, OP_TARGETDADDR( rsp, -8 ), OP_TARGETREG( temp2 ) ); 
-	mop->move( e, f->m, OP_TARGETDADDR( rsp, -12 ), OP_TARGETREG( temp3 ) ); 
+	// get arg passing registers
+	operand rargs[ RA_COUNT ];
+	prefer_nontemp_acquire_reg( mop, e, f->m, RA_COUNT, rargs );
 
-	release_specfic_temp( f->m, 0 );
-	release_specfic_temp( f->m, 1 );
-	release_specfic_temp( f->m, 2 );
+	// load metaframe section
+	mop->move( e, f->m, rargs[ RA_NR_RESULTS ], OP_TARGETDADDR( f->m->sp, meta_off ) );
+	mop->move( e, f->m, rargs[ RA_BASE ], OP_TARGETDADDR( f->m->sp, meta_off + 4 ) );
+	mop->move( e, f->m, rargs[ RA_CLOSURE ], OP_TARGETDADDR( f->m->sp, meta_off + 8 ) );
 
-	mop->add( e, f->m, osp, osp, OP_TARGETIMMED( -stack_frame_size( f->nr_locals ) ) ); 
-#endif
+	release_temp( mop, e, f->m );
+
+	// move stack to prior frame
+	operand sp = OP_TARGETREG( f->m->sp );
+	mop->add( e, f->m, sp, sp, OP_TARGETIMMED( stack_frame_size( f->nr_locals ) ) );
+
+	mop->ret( e, f->m );
 }
 
-#if 0
-// can overwrite rn because its not used afterwards 
-static void emit_nil_args( struct machine_ops* mop, struct emitter* e, struct frame* f, int basereg, int rn ){
-
-#if 0
-	basereg = ( basereg + 4 ) + ( 8 * rn );
-	end = ( basereg + 4 ) + ( 8 * nr_param );
-	while( basereg < end ){
-		sw nil, basereg
-		basereg += 8;
-	}
-	basereg = end - 4 - ( 8 * nr_param );
-#endif
-
-#if 0
-	operand iter = OP_TARGETREG( rn );
-	operand base = OP_TARGETREG( basereg );
-
-	e->ops->label_local( e, 0 );
-	mop->beq( e, f->m, iter, OP_TARGETIMMED( f->nr_params ), LBL_NEXT( 1 ) ); 
-	mop->add( e, f->m, base, iter
-	mop->add( e, f->m, iter, iter, OP_TARGETIMMED( 1 ) ); 
-	mop->b( e, f->m, LBL_NEXT( 0 ) );	
-	e->ops->label_local( e, 1 );
-#endif 
-}
-
-static void emit_copy_param( struct machine_ops* mop, struct emitter* me, struct frame* f, vreg_operand dst, int basereg, int idx ){
-	vreg_operand src;
-	src.value = OP_TARGETDADDR( basereg, idx * 8 );
-	src.type = OP_TARGETDADDR( basereg, idx * 8 + 4 );
-
-	assign( mop, me, f->m, dst, src );
-}
-
-/*
-* temp[0] = stack 
-* temp[1] = address to store results and address - 1 of argument location 
-* temp[2] = number of arguments available
-* temp[3] = number of ret args
-* stack is still in the old stack frame  
-*/
-static void emit_load_params( struct machine_ops* mop, struct emitter* me, struct frame* f, const int reg_value, const int reg_count ){
-	operand odst_value, odst_type, osrc_value, osrc_type;
-	vreg_operand dst;
-
-	// set unpassed arguments to nil  	
-	emit_nil_args( mop, me, f, reg_value, reg_count );
-
-	// TODO: add acquire_temp to emitter which takes machine as arg? 	
-	for( int i = 0; i < f->nr_params; i++ ){
-		dst = vreg_to_operand( f, i, false );
-		emit_copy_param( mop, me, f, dst, reg_value, i );
-	}	
-}
-#endif 
